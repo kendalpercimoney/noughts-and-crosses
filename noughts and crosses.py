ad8f5d7e2317d8267f3c8960c6e4e388871ecdef
Kendal Percimoney <kendal.percimoney@gmail.com>
2020-05-12 22:51:58 +0200

    Added random play by computer

--- noughts and crosses.py
+++ noughts and crosses.py
@@ -1,8 +1,10 @@
 import time
-
+import random
+from random import randint
 
 #variables
 print_sleep = (.03)
+end = False
 
 #play variables
 one = ("1")
@@ -67,6 +69,65 @@ def change_num(a): #assign the piece to its respective place
       b = input()
       change_num(b)
 
+def place_num(a): #assign the piece to its respective place
+    global one 
+    global two
+    global three
+    global four
+    global five
+    global six
+    global seven
+    global eight
+    global nine
+    global valueset
+    global x_o
+    
+    if (a == str(1)):
+      if (one != x_o and one != "O"):
+        one = ("O")
+      else:
+        place_num(str(randint (1, 9)))
+    elif (a == str(2)):
+      if (two != x_o and two != "O"):
+        two = ("O")
+      else:
+        place_num(str(randint (1, 9)))
+    elif (a == str(3)):
+      if (three != x_o and three != "O"):
+        three = ("O")
+      else:
+        place_num(str(randint (1, 9)))
+    elif (a == str(4)):
+      if (four != x_o and four != "O"):
+        four = ("O")
+      else:
+        place_num(str(randint (1, 9)))
+    elif (a == str(5)):
+      if (five != x_o and five != "O"):
+        five = ("O")
+      else:
+        place_num(str(randint (1, 9)))
+    elif (a == str(6)):
+      if (six != x_o and six != "O"):
+        six = ("O")
+      else:
+        place_num(str(randint (1, 9)))
+    elif (a == str(7)):
+      if (seven != x_o and seven != "O"):
+        seven = ("O")
+      else:
+        place_num(str(randint (1, 9)))
+    elif (a == str(8)):
+      if (eight != x_o and eight != "O"):
+        eight = ("O")
+      else:
+        place_num(str(randint (1, 9)))
+    elif (a == str(9)):
+      if (nine != x_o and nine != "O"):
+        nine = ("O")
+      else:
+        place_num(str(randint (1, 9)))
+
 
 
 def print_scrn():
@@ -102,6 +163,10 @@ def print_scrn():
 
 
 #def comp_play():
+  #if (two or four or six or eight == x_o):
+  
+
+    
 
 #main game:
 intro_scrn()
@@ -109,13 +174,22 @@ print("Would you like to play as X or O? (please enter one)")
 x_o = input()
 print("You have chosen " + x_o + " as the piece you would like to play with.")
 print_scrn()
-print("Please select the number of the block in which you would like to place your piece:")
+counter = 0
 
+while end == False:
+  print("Please select the number of the block in which you would like to place your piece:")
 
-#process
-input_num = input() #!add a check here
-change_num(input_num)
-#chk_win(input_num)
-print_scrn()
+  #process
+  input_num = input() #!add a check here
+  change_num(input_num)
+
+  place_num(str(randint(1,9)))
+  #chk_win(input_num)
+  print_scrn()
+
+  
+  counter = counter + 1
+  if (counter == 5):
+    end = True
 
 
