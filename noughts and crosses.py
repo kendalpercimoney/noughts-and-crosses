6ac0836d037e84eb81ec8e3289b7580e528d33e1
Kendal Percimoney <kendal.percimoney@gmail.com>
2020-05-23 00:22:33 +0200

    Update noughts and crosses.py
    Added win checking for 1, 5, 9 and "play again"

--- noughts and crosses.py
+++ noughts and crosses.py
@@ -1,10 +1,14 @@
 import time
 import random
 from random import randint
+import sys #for exit
 
 #variables
-print_sleep = (.03)
+print_sleep = (.00) #change to 0.3 for 'vibe'
 end = False
+playerWin = False
+computerWin = False
+repeats = 1
 
 #play variables
 one = ("1")
@@ -17,6 +21,28 @@ seven = ("7")
 eight = ("8")
 nine = ("9")
 valueset = False
+
+def reset_board():
+  global one 
+  global two
+  global three
+  global four
+  global five
+  global six
+  global seven
+  global eight
+  global nine
+  global playerWin
+  one = ("1")
+  two = ("2")
+  three = ("3")
+  four = ("4")
+  five = ("5")
+  six = ("6")
+  seven = ("7")
+  eight = ("8")
+  nine = ("9")
+  playerWin = False
 
 def intro_scrn(): #print intro screen
     print ()
@@ -69,7 +95,7 @@ def change_num(a): #assign the piece to its respective place
       b = input()
       change_num(b)
 
-def place_num(a): #assign the piece to its respective place
+def comp_play(a): #assign the piece to its respective place
     global one 
     global two
     global three
@@ -86,47 +112,47 @@ def place_num(a): #assign the piece to its respective place
       if (one != x_o and one != "O"):
         one = ("O")
       else:
-        place_num(str(randint (1, 9)))
+        comp_play(str(randint (1, 9)))
     elif (a == str(2)):
       if (two != x_o and two != "O"):
         two = ("O")
       else:
-        place_num(str(randint (1, 9)))
+        comp_play(str(randint (1, 9)))
     elif (a == str(3)):
       if (three != x_o and three != "O"):
         three = ("O")
       else:
-        place_num(str(randint (1, 9)))
+        comp_play(str(randint (1, 9)))
     elif (a == str(4)):
       if (four != x_o and four != "O"):
         four = ("O")
       else:
-        place_num(str(randint (1, 9)))
+        comp_play(str(randint (1, 9)))
     elif (a == str(5)):
       if (five != x_o and five != "O"):
         five = ("O")
       else:
-        place_num(str(randint (1, 9)))
+        comp_play(str(randint (1, 9)))
     elif (a == str(6)):
       if (six != x_o and six != "O"):
         six = ("O")
       else:
-        place_num(str(randint (1, 9)))
+        comp_play(str(randint (1, 9)))
     elif (a == str(7)):
       if (seven != x_o and seven != "O"):
         seven = ("O")
       else:
-        place_num(str(randint (1, 9)))
+        comp_play(str(randint (1, 9)))
     elif (a == str(8)):
       if (eight != x_o and eight != "O"):
         eight = ("O")
       else:
-        place_num(str(randint (1, 9)))
+        comp_play(str(randint (1, 9)))
     elif (a == str(9)):
       if (nine != x_o and nine != "O"):
         nine = ("O")
       else:
-        place_num(str(randint (1, 9)))
+        comp_play(str(randint (1, 9)))
 
 
 
@@ -162,34 +188,75 @@ def print_scrn():
 
 
 
-#def comp_play():
-  #if (two or four or six or eight == x_o):
+def chk_win():
+  global playerWin
+  if (one and five and nine  == x_o):
+    playerWin = True
   
 
     
 
 #main game:
-intro_scrn()
-print("Would you like to play as X or O? (please enter one)")
-x_o = input()
-print("You have chosen " + x_o + " as the piece you would like to play with.")
-print_scrn()
-counter = 0
+def main_game():
+  global end
+  global x_o
+  global repeats
 
-while end == False:
-  print("Please select the number of the block in which you would like to place your piece:")
-
-  #process
-  input_num = input() #!add a check here
-  change_num(input_num)
-
-  place_num(str(randint(1,9)))
-  #chk_win(input_num)
-  print_scrn()
-
+  reset_board() #resets values in case this is a 2nd game
   
-  counter = counter + 1
-  if (counter == 5):
-    end = True
+
+  intro_scrn()
+  print("Would you like to play as X or O? (please enter one)")
+  x_o = input()
+  print("You have chosen " + x_o + " as the piece you would like to play with.")
+  print_scrn()
+  counter = 0
+
+  while end == False:
+    print("Please select the number of the block in which you would like to place your piece:")
+
+    #process
+    input_num = input() #!add a check here
+    change_num(input_num)
+
+    comp_play(str(randint(1,9)))
+    chk_win()
+    print_scrn()
+
+    #replay if yes
+    if (playerWin == True): 
+      print("You won! Would you like to play again? Y or N")
+      
+      while repeats == 1:
+        answer = input()
+        if (answer == "Y"):
+          print ('Okay!')
+          main_game()
+        if(answer == "N"):
+          end = True
+        else:
+          print("Sorry! I don't understand that answer. Try again.")
+
+        
+    elif (computerWin == True):
+      print("Sorry, you lost. Would you like to play again? Y or N") 
+      answer = input()
+      if (answer == "y"):
+        print ('Okay!')
+        main_game()
+      elif(answer == "n"):
+        sys.exit()
 
 
+
+    
+    counter = counter + 1
+    if (counter == 5):
+      end = True
+
+
+#main:
+
+main_game()
+print("Thanks for playing!")
+sys.exit()
